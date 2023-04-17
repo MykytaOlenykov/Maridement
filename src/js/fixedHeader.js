08dcd94b9266d02a09fa4eed820f8b058fba7fd4
ForestDr <nikita2001395@gmail.com>
2023-04-17 19:20:41 +0300

    fix

--- src/js/fixedHeader.js
+++ src/js/fixedHeader.js
@@ -1,6 +1,14 @@
+import debounce from 'lodash.debounce';
+
 const header = document.querySelector('.js-header');
 
-document.addEventListener('scroll', () => {
+document.addEventListener(
+  'scroll',
+  debounce(onToggleFixeHeader, 300, { leading: true, trailing: true })
+);
+
+function onToggleFixeHeader() {
+  console.log(1);
   if (window.scrollY >= 1) {
     if (!header.classList.contains('page-header--fixed')) {
       header.classList.add('page-header--fixed');
@@ -10,7 +18,7 @@ document.addEventListener('scroll', () => {
     header.classList.remove('page-header--fixed');
     document.body.style.paddingTop = 0;
   }
-});
+}
 
 window.addEventListener('orientationchange', () => {
   setTimeout(() => {
