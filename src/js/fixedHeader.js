f10785141e444a52978443fa5d4b2de97a205070
ForestDr <nikita2001395@gmail.com>
2023-04-17 18:55:32 +0300

    add fix

--- src/js/fixedHeader.js
+++ src/js/fixedHeader.js
@@ -1,25 +1,16 @@
-import debounce from 'lodash.debounce';
-
 const header = document.querySelector('.js-header');
 
-document.addEventListener(
-  'scroll',
-  debounce(
-    () => {
-      if (window.scrollY >= 1) {
-        if (!header.classList.contains('page-header--fixed')) {
-          header.classList.add('page-header--fixed');
-          document.body.style.paddingTop = `${header.clientHeight}px`;
-        }
-      } else {
-        header.classList.remove('page-header--fixed');
-        document.body.style.paddingTop = 0;
-      }
-    },
-    300,
-    { leading: true, trailing: false }
-  )
-);
+document.addEventListener('scroll', () => {
+  if (window.scrollY >= 1) {
+    if (!header.classList.contains('page-header--fixed')) {
+      header.classList.add('page-header--fixed');
+      document.body.style.paddingTop = `${header.clientHeight}px`;
+    }
+  } else {
+    header.classList.remove('page-header--fixed');
+    document.body.style.paddingTop = 0;
+  }
+});
 
 window.addEventListener('orientationchange', () => {
   setTimeout(() => {
