ae74b383d29404214b2c14808de792dfd3fe0afe
ForestDr <nikita2001395@gmail.com>
2023-04-17 17:12:40 +0300

    add debounce

--- src/js/fixedHeader.js
+++ src/js/fixedHeader.js
@@ -1,11 +1,21 @@
-const header = document.querySelector(".js-header");
+import debounce from 'lodash.debounce';
 
-document.addEventListener("scroll", () => {
-  if (window.scrollY >= 1) {
-    header.classList.add("page-header--fixed");
-    document.body.style.paddingTop = `${header.clientHeight}px`;
-  } else {
-    header.classList.remove("page-header--fixed");
-    document.body.style.paddingTop = 0;
-  }
-});
+const header = document.querySelector('.js-header');
+
+document.addEventListener(
+  'scroll',
+  debounce(
+    () => {
+      console.log(1);
+      if (window.scrollY >= 1) {
+        header.classList.add('page-header--fixed');
+        document.body.style.paddingTop = `${header.clientHeight}px`;
+      } else {
+        header.classList.remove('page-header--fixed');
+        document.body.style.paddingTop = 0;
+      }
+    },
+    300,
+    { leading: true }
+  )
+);
