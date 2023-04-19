4981caefd8101f13c942c74e21c19f07c98a9d8f
ForestDr <nikita2001395@gmail.com>
2023-04-19 17:40:54 +0300

    add site-nav__link--active

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,14 +2,15 @@ const navEl = document.querySelector('.js-nav');
 const mobileMenu = document.querySelector('.js-menu-container');
 
 navEl.addEventListener('click', e => {
-  // btn.classList.add('button--active');
-
-  // setTimeout(() => {
-  //   btn.classList.remove('button--active');
-  // }, 100);
   const targetEl = e.target;
 
   if (targetEl.classList.contains('js-link')) {
+    targetEl.classList.add('site-nav__link--active');
+
+    setTimeout(() => {
+      targetEl.classList.remove('site-nav__link--active');
+    }, 100);
+
     document.body.classList.remove('body-lock');
 
     if (mobileMenu.classList.contains('menu-container--is-open')) {
