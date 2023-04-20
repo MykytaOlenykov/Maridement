e59382c3743d5751dd339e1c32bda39c1250e948
ForestDr <nikita2001395@gmail.com>
2023-04-21 00:14:44 +0300

    test fix

--- src/js/toggleFAQ.js
+++ src/js/toggleFAQ.js
@@ -4,11 +4,11 @@ faqList.addEventListener('click', e => {
   const targetEl = e.target.closest('.js-faq-button');
 
   if (targetEl) {
-    targetEl.classList.add('faq-list__button--active');
+    // targetEl.classList.add('faq-list__button--active');
 
-    setTimeout(() => {
-      targetEl.classList.remove('faq-list__button--active');
-    }, 100);
+    // setTimeout(() => {
+    //   targetEl.classList.remove('faq-list__button--active');
+    // }, 100);
 
     targetEl
       .closest('.js-faq-item')
