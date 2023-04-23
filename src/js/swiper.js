6759fdd6aa1338836ef0414308fa34fa5705997f
ForestDr <nikita2001395@gmail.com>
2023-04-23 21:31:38 +0300

    add fix

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,6 @@
 import Swiper, { Navigation, Keyboard } from 'swiper';
 
 new Swiper('.feedback-slider', {
-  autoHeight: true,
   slidesPerView: 1,
   spaceBetween: 30,
   breakpoints: {
@@ -10,22 +9,18 @@ new Swiper('.feedback-slider', {
       slidesPerView: 1,
     },
     1280: {
-      autoHeight: false,
       spaceBetween: 50,
       slidesPerView: 2.23,
     },
     1440: {
-      autoHeight: false,
       spaceBetween: 50,
       slidesPerView: 2.23,
     },
     1450: {
-      autoHeight: false,
       spaceBetween: 50,
       slidesPerView: 2.5,
     },
     1700: {
-      autoHeight: false,
       spaceBetween: 50,
       slidesPerView: 3,
     },
