0356e10a6d9e3b9d2a5dba252eb2716ee8fe372d
ForestDr <nikita2001395@gmail.com>
2023-05-28 20:46:28 +0300

    add header component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import './js/IIFE.js';
+import './js/components/header';
+import './js/components/faq/index.js';
+
 import './js/swiper.js';
 import './js/feedbackModal.js';
-import './js/mobileMenu.js';
 import './js/activeBtn.js';
-import './js/nav.js';
-import './js/activeHeader.js';
 import './js/activeContactLink.js';
-
-import './js/components/faq/index.js';
--- src/js/IIFE.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const header = document.querySelector('.js-header');
-
-  if (window.scrollY >= 1) {
-    header.classList.add('page-header--active');
-  }
-})();
